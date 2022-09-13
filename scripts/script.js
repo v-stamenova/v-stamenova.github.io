30c6116d8fc767459858f1edd292ec3e316ac89c
valst3 <valeria.stamenova3@gmail.com>
2022-09-14 01:48:26 +0200

    Create a legend and do automatic coloring

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 let i = 0;
 let passing = 5.5;
 let collectedCredits = 0.0;
+let gradeId = "grade-";
 
 function calculateTotalCredits() {
     let totalCredits = 60;
 
-    let gradeId = "grade-";
     let creditsId = "credits-";
 
     for (let i = 1; i < 11; i++) {
@@ -25,9 +25,17 @@ function calculateTotalCredits() {
                 sumGrades += Number(document.getElementById(gradeId + i + "." + j).textContent);
             }
 
-            if (isExamPassed(sumGrades / 2)) {
+            if (i == 8 || i == 9) {
+                additional = 1;
+            }
+            else {
+                additional = 0;
+            }
+
+            if (isExamPassed(sumGrades / (2 + additional))) {
                 collectedCredits += Number(document.getElementById(creditsId + i).textContent);
             }
+            colorCells(sumGrades / (2 + additional), i, 2 + additional);
         }
         else {
             grade = document.getElementById(gradeId + i).textContent;
@@ -35,11 +43,38 @@ function calculateTotalCredits() {
             if (isExamPassed(grade)) {
                 collectedCredits += Number(document.getElementById(creditsId + i).textContent);
             }
+            colorCells(grade, i, 1);
         }
 
     }
-
     move();
+}
+
+function colorCells(grade, index, cellCount) {
+    if (grade) {
+        if (grade >= passing) {
+            if (cellCount > 1) {
+                alert("so far so good");
+
+                for (let i = 1; i < cellCount + 1; i++) {
+                    document.getElementById(gradeId + index + "." + i).style.backgroundColor = "#006D77";
+                }
+            }
+            else {
+                document.getElementById(gradeId + index).style.backgroundColor = "#006D77";
+            }
+        }
+        else {
+            if (cellCount > 1) {
+                for (let i = 1; i < cellCount + 1; i++) {
+                    document.getElementById(gradeId + index + "." + i).style.backgroundColor = "#ce5374";
+                }
+            }
+            else {
+                document.getElementById(gradeId + index).style.backgroundColor = "#ce5374";
+            }
+        }
+    }
 }
 
 function isExamPassed(grade) {
@@ -55,20 +90,19 @@ function isExamPassed(grade) {
 function move() {
     let percent = (100.0 / 60.0 * collectedCredits);
 
-    if (i == 0) {
-        i = 1;
-        let elem = document.getElementById("myBar");
-        let width = 1;
-        let id = setInterval(frame, 10);
+    i = 1;
+    let elem = document.getElementById("myBar");
+    let width = 1;
+    let id = setInterval(frame, 30);
 
-        function frame() {
-            if (width >= percent) {
-                clearInterval(id);
-                i = 0;
-            } else {
-                width++;
-                elem.style.width = width + "%";
-            }
+    function frame() {
+        if (width >= percent) {
+            clearInterval(id);
+            i = 0;
+        } else {
+            width++;
+            elem.style.width = width + "%";
         }
     }
+
 }
